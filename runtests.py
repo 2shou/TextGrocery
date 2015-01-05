6f74c3b0054fecae2d9e56ac2862df9a5c4bdee3
2shou <gavin.zgz@gmail.com>
2015-01-05 17:23:58 +0800

    add unittest

--- runtests.py
+++ runtests.py
@@ -1,10 +1,24 @@
 # coding: utf-8
 
 import jieba
+import unittest
 
 from tgrocery import Grocery
 
 
-grocery = Grocery('test', tokenizer=jieba.cut)
-grocery.train('sample/train_ch.txt')
-grocery.predict('考生必读：新托福写作考试评分标准')
+class GroceryTestCase(unittest.TestCase):
+    def setUp(self):
+        self.grocery = Grocery('test', tokenizer=jieba.cut)  # To support Chinese short-text classification
+
+    def test_train(self):
+        self.grocery.train('sample/train_ch.txt')
+        assert self.grocery.get_load_status()
+
+    def test_predict(self):
+        assert self.grocery.predict('考生必读：新托福写作考试评分标准') == 'education'
+
+    def test_test(self):
+        self.grocery.test('sample/test_ch.txt')
+
+if __name__ == 'main':
+    unittest.main()
