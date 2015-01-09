ab283951607989a7f73ca754c1a22f085b74df44
2shou <gavin.zgz@gmail.com>
2015-01-09 15:41:54 +0800

    rewrite python api of libshorttext

--- runtests.py
+++ runtests.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-import jieba
 import unittest
 
 from tgrocery import Grocery
@@ -10,16 +9,10 @@ class GroceryTestCase(unittest.TestCase):
     def setUp(self):
         self.grocery = Grocery('test')
 
-    def test_train(self):
+    def test_main(self):
         self.grocery.train('sample/train_ch.txt')
         assert self.grocery.get_load_status()
         assert self.grocery.predict('考生必读：新托福写作考试评分标准') == 'education'
-
-    def test_predict(self):
-        pass
-
-        # def test_test(self):
-        # self.grocery.test('sample/test_ch.txt')
 
 
 if __name__ == 'main':
